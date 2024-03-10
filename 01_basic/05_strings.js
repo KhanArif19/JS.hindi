f4737a7e7edff29197b261cacea9642ac2247027
KhanArif19 <162313940+KhanArif19@users.noreply.github.com>
2024-03-10 07:27:18 +0000

    strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Arif"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Arifnk')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
