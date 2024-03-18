1ccf5b8ae61b9a3d3ab090b048e5b0734d79ede1
KhanArif19 <162313940+KhanArif19@users.noreply.github.com>
2024-03-18 19:12:42 +0000

    functions

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,35 @@
+
+function sayMyName(){
+    console.log("A");
+    console.log("R");
+    console.log("I");
+    console.log("F");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+return number1 + number2
+}
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+}
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Arif"))
+console.log(loginUserMessage("Arif"));
