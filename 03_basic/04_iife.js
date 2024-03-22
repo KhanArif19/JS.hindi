f8972401a86e8bfc3fa81f265e1b3d87ab6584f5
KhanArif19 <162313940+KhanArif19@users.noreply.github.com>
2024-03-22 13:09:03 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Inovked Function Expressions (IIFE)
+
+
+(function chai() {
+    // named IIFE
+    console.log(`DB CONNECTED`);
+}) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+}) ('Arif')
