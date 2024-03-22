5557401e5d43be3f6ef9bcd948d8fb0022efbf5b
KhanArif19 <162313940+KhanArif19@users.noreply.github.com>
2024-03-22 09:52:59 +0000

    arrow

---
+++ 03_basic/03_Arrow.js
@@ -0,0 +1,22 @@
+const user = {
+    username: "Arif",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai() {
+//     let username = "Arif"
+//     console.log(this.username);
+// }
+
+// chai()
